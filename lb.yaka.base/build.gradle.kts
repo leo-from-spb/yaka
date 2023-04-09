69ee883f75df422cfd39887f536f0854b68a93c2
Leonid Bushuev <leonidos@gmail.com>
2023-04-09 20:49:06 +0200

    Publish artifacts to Space

--- lb.yaka.base/build.gradle.kts
+++ lb.yaka.base/build.gradle.kts
@@ -3,11 +3,15 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     `java-library`
     kotlin("jvm")
+    `maven-publish`
 }
 
 
 base.archivesBaseName = "yaka-base"
 val moduleName by extra("lb.yaka.base")
+
+val spaceLogin: String by project
+val spaceToken: String by project
 
 
 java {
@@ -34,5 +38,26 @@ tasks {
     compileJava {
         inputs.property("moduleName", moduleName)
         options.compilerArgs = listOf("--patch-module", "$moduleName=${sourceSets.main.get().output.asPath}", "-Xlint:deprecation")
+    }
+}
+
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "lb.yaka"
+            artifactId = "lb.yaka.base"
+            version = version
+            from(components["java"])
+        }
+    }
+    repositories {
+        maven {
+            url = uri("https://packages.jetbrains.team/maven/p/dm/maven")
+            credentials {
+                username = spaceLogin
+                password = spaceToken
+            }
+        }
     }
 }
